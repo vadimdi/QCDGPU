ef518b5c568c0da89d87b49828e4850198336721
Vadim Demchik <vadimdi@yahoo.com>
2014-04-16 15:35:46 +0300

    update

--- SU(N)/suncl/suncpu.cpp
+++ SU(N)/suncl/suncpu.cpp
@@ -2,14 +2,14 @@
  * @file     suncpu.cpp
  * @author   Vadim Demchik <vadimdi@yahoo.com>,
  * @author   Natalia Kolomoyets <rknv7@mail.ru>
- * @version  1.0
+ * @version  1.4
  *
  * @brief    [QCDGPU]
  *           Run of CPU check procedures
  *
  * @section  LICENSE
  *
- * Copyright (c) 2013, Vadim Demchik, Natalia Kolomoyets
+ * Copyright (c) 2013, 2014 Vadim Demchik, Natalia Kolomoyets
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification,
