9f3a81bd6c4854dfef41085b5a047611cba91557
Vadim Demchik <vadimdi@yahoo.com>
2014-04-16 15:24:16 +0300

    update

--- SU(N)/clinterface/platform.h
+++ SU(N)/clinterface/platform.h
@@ -2,14 +2,14 @@
  * @file     platform.h
  * @author   Vadim Demchik <vadimdi@yahoo.com>,
  * @author   Natalia Kolomoyets <rknv7@mail.ru>
- * @version  1.0
+ * @version  1.4
  *
  * @brief    [QCDGPU]  
  *           External dependecies corresponding to user's operating system
  *
  * @section  LICENSE
  *
- * Copyright (c) 2013, Vadim Demchik, Natalia Kolomoyets
+ * Copyright (c) 2013, 2014 Vadim Demchik, Natalia Kolomoyets
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification,
