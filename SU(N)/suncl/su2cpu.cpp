ab69f38f1f5273efd77cede670ed80ad20df0340
Vadim Demchik <vadimdi@yahoo.com>
2014-08-20 18:21:50 +0300

    update

--- SU(N)/suncl/su2cpu.cpp
+++ SU(N)/suncl/su2cpu.cpp
@@ -2,7 +2,7 @@
  * @file     suncpu.cpp
  * @author   Vadim Demchik <vadimdi@yahoo.com>,
  * @author   Natalia Kolomoyets <rknv7@mail.ru>
- * @version  1.4
+ * @version  1.5
  *
  * @brief    [QCDGPU]
  *           Procedures for host simulations (SU(2) gauge group)
